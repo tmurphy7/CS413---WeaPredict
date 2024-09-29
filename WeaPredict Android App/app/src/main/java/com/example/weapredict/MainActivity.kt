d742db45994f31050d79d77a2bf54dabfded5089
Gbell26 <107581827+Gbell26@users.noreply.github.com>
2024-09-29 19:34:49 -0400

    rendering 7 day scroll layout

--- WeaPredict Android App/app/src/main/java/com/example/weapredict/MainActivity.kt
+++ WeaPredict Android App/app/src/main/java/com/example/weapredict/MainActivity.kt
@@ -39,6 +39,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
