c43fa6fd6e50be594cc94a9ec6994001ced214b5
Gbell26 <107581827+Gbell26@users.noreply.github.com>
2024-10-19 15:36:05 -0400

    inserted current weather data into first day & removed weather type strings

--- WeaPredict Android App/app/src/main/java/com/example/weapredict/MainActivity.kt
+++ WeaPredict Android App/app/src/main/java/com/example/weapredict/MainActivity.kt
@@ -168,180 +168,181 @@ class MainActivity : ComponentActivity() {
     private fun updateWeatherString(newWeather: String) {
         weatherStringState = newWeather
     }
-}
-@Composable
-fun DisplayDays(){
-    //Display the weather for 7 day forecast
-    Row(modifier = Modifier
-        .horizontalScroll(rememberScrollState())
-        .fillMaxWidth()) {
+    @Composable
+    fun DisplayDays(){
+        //Display the weather for 7 day forecast
+        Row(modifier = Modifier
+            .horizontalScroll(rememberScrollState())
+            .fillMaxWidth()) {
 
-        val weatherObjectDaysList = getDays()
-        
-        for (day in weatherObjectDaysList) {
+            val weatherObjectDaysList = getDays()
+
+            for (day in weatherObjectDaysList) {
+                //set image for weather type
+                val weatherCondition = when (day.weather_type) {
+                    "Clear sky" -> R.drawable.sun
+                    "Partly cloudy" -> R.drawable.parlycloudy
+                    "Foggy" -> R.drawable.fog
+                    "Drizzle" -> R.drawable.lightrain
+                    "Rain showers","Rain" -> R.drawable.heavyrain
+                    "Snow","Snow showers","Snow grains" -> R.drawable.snow
+                    "Thunderstorm" -> R.drawable.storm
+                    "Thunderstorm with hail" -> R.drawable.stormwithheavyrain
+                    else -> R.drawable.sun
+                }
+                Column(modifier = Modifier.padding(8.dp)) {
+                    Box(modifier = Modifier.height(50.dp)) {
+                        Image(
+                            modifier = Modifier.size(60.dp),
+                            contentDescription = "Weather Image",
+                            contentScale = ContentScale.Crop,
+                            painter = painterResource(weatherCondition)
+                        )
+                    }
+                    Text(
+                        text = day.day + "\n" + day.temperature,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun DisplayHours() {
+        Row(modifier = Modifier
+            .horizontalScroll(rememberScrollState())
+            .fillMaxWidth()) {
+
+            val weatherObjectHourList = getHours()
             //set image for weather type
-            val weatherCondition = when (day.weather_type) {
-                "Clear \nsky" -> R.drawable.sun
-                "Partly \ncloudy" -> R.drawable.parlycloudy
-                "Foggy" -> R.drawable.fog
-                "Drizzle" -> R.drawable.lightrain
-                "Rain showers","Rain" -> R.drawable.heavyrain
-                "Snow","Snow showers","Snow grains" -> R.drawable.snow
-                "Thunderstorm" -> R.drawable.storm
-                "Thunderstorm with hail" -> R.drawable.stormwithheavyrain
-                else -> R.drawable.sun
-            }
-            Column(modifier = Modifier.padding(8.dp)) {
-                Box(modifier = Modifier.height(50.dp)) {
-                    Image(
-                        modifier = Modifier.size(60.dp),
-                        contentDescription = "Weather Image",
-                        contentScale = ContentScale.Crop,
-                        painter = painterResource(weatherCondition)
+            for (hour in weatherObjectHourList) {
+                val weatherCondition = when (hour.weather_type) {
+                    "Clear sky" -> R.drawable.sun
+                    "Partly cloudy" -> R.drawable.parlycloudy
+                    "Foggy" -> R.drawable.fog
+                    "Drizzle" -> R.drawable.lightrain
+                    "Rain showers","Rain" -> R.drawable.heavyrain
+                    "Snow","Snow showers","Snow grains" -> R.drawable.snow
+                    "Thunderstorm" -> R.drawable.storm
+                    "Thunderstorm with hail" -> R.drawable.stormwithheavyrain
+                    else -> R.drawable.sun
+                }
+
+                Column(modifier = Modifier.padding(8.dp)) {
+                    Box(modifier = Modifier.height(50.dp)) {
+                        Image(
+                            modifier = Modifier.size(60.dp),
+                            contentDescription = "Weather Image",
+                            contentScale = ContentScale.Crop,
+                            painter = painterResource(weatherCondition)
+                        )
+                    }
+                    Text(
+                        text = hour.hour + "\n" + hour.temperature,
+                        modifier = Modifier.padding(8.dp)
                     )
                 }
-                Text(
-                    text = day.day + "\n" + day.temperature + "\n" + day.weather_type,
-                    modifier = Modifier.padding(8.dp)
-                )
             }
         }
+
     }
-}
+    fun getDays(): List<WeatherManager.WeatherInstance>{
+        //create 7 weather objects and make predictions to fill weather
+        val calendar = Calendar.getInstance()
+        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1
 
-@Composable
-fun DisplayHours() {
-    Row(modifier = Modifier
-        .horizontalScroll(rememberScrollState())
-        .fillMaxWidth()) {
-
-        val weatherObjectHourList = getHours()
-        //set image for weather type
-        for (hour in weatherObjectHourList) {
-            val weatherCondition = when (hour.weather_type) {
-                "Clear \nsky" -> R.drawable.sun
-                "Partly \ncloudy" -> R.drawable.parlycloudy
-                "Foggy" -> R.drawable.fog
-                "Drizzle" -> R.drawable.lightrain
-                "Rain showers","Rain" -> R.drawable.heavyrain
-                "Snow","Snow showers","Snow grains" -> R.drawable.snow
-                "Thunderstorm" -> R.drawable.storm
-                "Thunderstorm with hail" -> R.drawable.stormwithheavyrain
-                else -> R.drawable.sun
-            }
-
-            Column(modifier = Modifier.padding(8.dp)) {
-                Box(modifier = Modifier.height(50.dp)) {
-                    Image(
-                        modifier = Modifier.size(60.dp),
-                        contentDescription = "Weather Image",
-                        contentScale = ContentScale.Crop,
-                        painter = painterResource(weatherCondition)
-                    )
-                }
-                Text(
-                    text = hour.hour + "\n" + hour.temperature + "\n" + hour.weather_type,
-                    modifier = Modifier.padding(8.dp)
-                )
-            }
+        var daysList = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+        //Split the list to start from today
+        if(dayOfWeek != 0){
+            val daysListSecondHalf = daysList.subList(0,dayOfWeek - 1)
+            val daysListFirstHalf = daysList.subList(dayOfWeek - 1,daysList.size)
+            daysList = daysListFirstHalf + daysListSecondHalf
         }
-    }
 
-}
-fun getDays(): List<WeatherManager.WeatherInstance>{
-    //create 7 weather objects and make predictions to fill weather
-    val calendar = Calendar.getInstance()
-    val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1
+        var weatherObjectList: List<WeatherManager.WeatherInstance> = emptyList()
 
-    var daysList = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
-    //Split the list to start from today
-    if(dayOfWeek != 0){
-        val daysListSecondHalf = daysList.subList(0,dayOfWeek - 1)
-        val daysListFirstHalf = daysList.subList(dayOfWeek - 1,daysList.size)
-        daysList = daysListFirstHalf + daysListSecondHalf
-    }
+        //add first day to list
+        val temperature = currentWeatherData.temperature
+        val skies = currentWeatherData.weather_type
+        val todaysWeather = WeatherManager.WeatherInstance(weather_type = skies,temperature = temperature, day = daysList[0])
 
-    var weatherObjectList: List<WeatherManager.WeatherInstance> = emptyList()
+        //drop first element from dayslist
+        daysList = daysList.drop(1)
 
-    //add first day to list
-    val temperature = 68.0 // Get temp and conditions from api
-    val skies = "Clear \nsky"
-    val todaysWeather = WeatherManager.WeatherInstance(weather_type = skies,temperature = temperature, day = daysList[0])
-
-    //drop first element from dayslist
-    daysList = daysList.drop(1)
-
-    weatherObjectList = weatherObjectList + todaysWeather
+        weatherObjectList = weatherObjectList + todaysWeather
 //for loop through days creating weather objects and adding to list
-    for(x in daysList){
-        val nextDayTemp = 70.0 // predict temperature
-        val nextDaySkies = "Rain" // predict conditions
-        val nextDayWeather = WeatherManager.WeatherInstance(weather_type = nextDaySkies,temperature = nextDayTemp, day = x)
-        weatherObjectList = weatherObjectList + nextDayWeather
+        for(x in daysList){
+            val nextDayTemp = 70.0 // predict temperature
+            val nextDaySkies = "Rain" // predict conditions
+            val nextDayWeather = WeatherManager.WeatherInstance(weather_type = nextDaySkies,temperature = nextDayTemp, day = x)
+            weatherObjectList = weatherObjectList + nextDayWeather
+        }
+
+        return weatherObjectList
     }
 
-    return weatherObjectList
-}
+    fun getHours(): List<WeatherManager.WeatherInstance>{
+        var hoursList = listOf("12AM","1AM","2AM","3AM","4AM","5AM","6AM","7AM","8AM","9AM","10AM","11AM","12PM","1PM",
+            "2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM")
 
-fun getHours(): List<WeatherManager.WeatherInstance>{
-    var hoursList = listOf("12AM","1AM","2AM","3AM","4AM","5AM","6AM","7AM","8AM","9AM","10AM","11AM","12PM","1PM",
-        "2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM")
+        //get current hour
+        val currentTime = Calendar.getInstance()
+        val currentHour = currentTime.get(Calendar.HOUR_OF_DAY)
 
-    //get current hour
-    val currentTime = Calendar.getInstance()
-    val currentHour = currentTime.get(Calendar.HOUR_OF_DAY)
+        //get current weather
+        val temperature = currentWeatherData.temperature
+        val skies = currentWeatherData.weather_type
 
-    //get current weather
-    val temperature = 68.0
-    val skies = "Partly \ncloudy"
+        //sort list of hours into correct order
+        if(currentHour != 0){
+            val hoursListSecondHalf = hoursList.subList(0,currentHour)
+            val hoursListFirstHalf = hoursList.subList(currentHour,hoursList.size)
+            hoursList = hoursListFirstHalf + hoursListSecondHalf
+        }
 
-    //sort list of hours into correct order
-    if(currentHour != 0){
-        val hoursListSecondHalf = hoursList.subList(0,currentHour)
-        val hoursListFirstHalf = hoursList.subList(currentHour,hoursList.size)
-        hoursList = hoursListFirstHalf + hoursListSecondHalf
+        var weatherObjectList: List<WeatherManager.WeatherInstance> = emptyList()
+        //for loop through days creating weather objects and adding to list
+        val todaysWeather = WeatherManager.WeatherInstance(weather_type = skies,temperature = temperature, hour = hoursList[0])
+
+        //drop first element from hourslist
+        hoursList = hoursList.drop(1)
+
+
+        weatherObjectList = weatherObjectList + todaysWeather
+
+        for(x in hoursList){
+            val nextDayTemp = 70.0 // predict temperature
+            val nextDaySkies = "Rain" // predict conditions
+            val nextDayWeather = WeatherManager.WeatherInstance(weather_type = nextDaySkies,temperature = nextDayTemp, hour = x)
+            weatherObjectList = weatherObjectList + nextDayWeather
+        }
+
+        return weatherObjectList
     }
 
-    var weatherObjectList: List<WeatherManager.WeatherInstance> = emptyList()
-    //for loop through days creating weather objects and adding to list
-    val todaysWeather = WeatherManager.WeatherInstance(weather_type = skies,temperature = temperature, hour = hoursList[0])
 
-    //drop first element from hourslist
-    hoursList = hoursList.drop(1)
-
-
-    weatherObjectList = weatherObjectList + todaysWeather
-
-    for(x in hoursList){
-        val nextDayTemp = 70.0 // predict temperature
-        val nextDaySkies = "Rain" // predict conditions
-        val nextDayWeather = WeatherManager.WeatherInstance(weather_type = nextDaySkies,temperature = nextDayTemp, hour = x)
-        weatherObjectList = weatherObjectList + nextDayWeather
+    @Composable
+    fun DisplayString(string: String, modifier: Modifier = Modifier) {
+        Text(
+            text = string,
+            modifier = modifier
+        )
     }
 
-    return weatherObjectList
-}
-
-
-@Composable
-fun DisplayString(string: String, modifier: Modifier = Modifier) {
-    Text(
-        text = string,
-        modifier = modifier
-    )
-}
-
-@Composable
-fun FindLocationButton(onClick: () -> Unit) {
-    Button(onClick = { onClick() }) {
-        Text("Refresh Data")
+    @Composable
+    fun FindLocationButton(onClick: () -> Unit) {
+        Button(onClick = { onClick() }) {
+            Text("Refresh Data")
+        }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeaPredictTheme {
-        DisplayString("Location Unknown")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        WeaPredictTheme {
+            DisplayString("Location Unknown")
+        }
     }
+
 }
